9664677d49845bd78c7db008c4cc1726206c67b3
DirectorLord <thelord161203@gmail.com>
2025-07-24 14:29:37 +0300

    Add project files.

---
+++ OOP05/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OOP05;
+
+    using System;
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        Console.WriteLine("test");
+        }
+    }
